86b40b391c7746090e60592ce6eb0c38a725fa8a
Philip Cohn <philip.cohn@speakap.nl>
2020-02-09 18:31:23 +0100

    basic done

--- music-vis-react-app/src/App.js
+++ music-vis-react-app/src/App.js
@@ -8,26 +8,37 @@ import { motion } from "framer-motion"
 
 function PulseCircle(props) {
 
-
   return (
-    <svg  viewBox="0 0 100 100">
+    <g>
       <motion.circle className="pulse-anim" 
         cx={props.x} cy={props.y} r="10"
 
          
-        whileTap={{ 
+        animate={{ 
           scale: [1, 1.4, 1, 1],
-          x: props.x,
-          y: props.y
         }}
         transition={{
           duration: 1,
           ease: "easeOut",
           times: [0, 0.2, 0.8, 1],
         }}
+        onAnimationComplete={props.onAnimationComplete}
       />
-    </svg>
+    </g>
   );
+}
+
+function PulseCircles(props) {
+  if (props.pulseCircles.length > 0){
+    const pulseCircles = props.pulseCircles.map((pulseCircle, i) => {
+      return (
+        <PulseCircle key={i} x={pulseCircle.x} y={pulseCircle.y} onAnimationComplete={props.onAnimationComplete} />
+      )
+    })
+    return pulseCircles;
+  } else {
+    return null;
+  }
 }
 
 class ArtBoard extends React.Component {
@@ -35,21 +46,44 @@ class ArtBoard extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      x: 0,
-      y: 0
+      pulseCircles: []
     }
-    
-    this.onMouseMove = this.onMouseMove.bind(this);
+    this.artboardRef = React.createRef();
+    this.onMouseDown = this.onMouseDown.bind(this);
   }
 
-  onMouseMove(e) {
-    this.setState({ x: e.nativeEvent.offsetX, y: e.nativeEvent.offsetY });
+  alert_coords(evt) {
+    let svg = this.artboardRef.current;
+    let pt = svg.createSVGPoint();  
+
+    pt.x = evt.clientX;
+    pt.y = evt.clientY;
+
+    // The cursor point, translated into svg coordinates
+    var cursorpt =  pt.matrixTransform(svg.getScreenCTM().inverse());
+    console.log("(" + cursorpt.x + ", " + cursorpt.y + ")");
+    return({
+      x: cursorpt.x, 
+      y: cursorpt.y
+    })
+  }
+
+  onMouseDown(e) {
+    const newPulseCircle = [{x: this.alert_coords(e).x, y: this.alert_coords(e).y }]
+    this.setState({ pulseCircles: this.state.pulseCircles.concat(newPulseCircle) });
+    console.log(this.state);
+  }
+
+  onAnimationComplete(){
+    console.log('done');
   }
 
   render () {
     return (
-      <div className="artboard" onMouseMove={this.onMouseMove}> 
-        <PulseCircle x={this.state.x} y={this.state.y}/>
+      <div className="artboard-container" onMouseDown={this.onMouseDown}> 
+        <svg  viewBox="0 0 100 100" ref={this.artboardRef}>
+          <PulseCircles pulseCircles={this.state.pulseCircles} onAnimationComplete={this.onAnimationComplete}/>
+        </svg>
       </div>
     )
   }
