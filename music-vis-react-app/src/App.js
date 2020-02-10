7cd531b4194886fc401291e6a956bcf5fee0169f
Philip Cohn <philip.cohn@speakap.nl>
2020-02-10 02:53:40 +0100

    more done

--- music-vis-react-app/src/App.js
+++ music-vis-react-app/src/App.js
@@ -3,58 +3,71 @@ import './App.css';
 import { motion } from "framer-motion"
 
 
-// https://codepen.io/jakebown/pen/weoVxg
-// https://codepen.io/CarlosEME/pen/XWWpVMp
-
 class PulseCircle extends React.Component  {
 
   constructor(props){
     super(props)
-    this.onAnimationComplete = props.onAnimationComplete;
     this.index = props.index;
   }
 
-  onComplete = () => {
-    this.onAnimationComplete(this.index);
-  }
 
   render() {
     return (
       <g>
-        <motion.circle className="pulse-anim" 
-          cx={this.props.x} cy={this.props.y} r="10"
-  
-          animate={{ 
-            scale: [1, 1.4, 1, 1],
-          }}
-          transition={{
-            duration: 1,
-            ease: "easeOut",
-            times: [0, 0.2, 0.8, 1],
-          }}
-        />
+          <motion.circle className="pulse-anim" 
+            stroke={this.props.colour}
+            cx={this.props.x} cy={this.props.y} r="10"
+            initial= {{scale: .1,opacity: 1}}
+            animate={{ 
+              scale: [.1,8],
+              opacity: [1,0]
+            }}
+
+            transition={{
+              duration: 20,
+              ease: "easeOut",
+            }}
+          />
       </g>
     );
   }
 
 }
 
-function PulseCircles(props) {
-  if (props.pulseCircles.length > 0){
-    const pulseCircles = props.pulseCircles.map((pulseCircle, index) => {
-      return (
-        <PulseCircle 
-          key={index} 
-          x={pulseCircle.x} 
-          y={pulseCircle.y} 
-          index={index}
-        />
-      )
-    })
-    return pulseCircles;
-  } else {
-    return null;
+class PulseCircles extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.colours = [
+      '#3D6CFF',
+      '#3890E8',
+      '#4AD5FF',
+      '#38E8E2',
+      '#3DFFC2'
+    ]
   }
+
+  render(){
+    let colourIndex = 0;
+    if (this.props.pulseCircles.length > 0){
+      const pulseCircles = this.props.pulseCircles.map((pulseCircle, index) => {
+        colourIndex = colourIndex >= this.colours.length-1 ? 0 : colourIndex +1;
+        return (
+          <PulseCircle 
+            key={index} 
+            x={pulseCircle.x} 
+            y={pulseCircle.y} 
+            index={index}
+            colour={this.colours[colourIndex]}
+          />
+        )
+      })
+      return pulseCircles;
+    } else {
+      return null;
+    }
+  }
+  
 }
 
 class ArtBoard extends React.Component {
@@ -62,7 +75,7 @@ class ArtBoard extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      pulseCircles: []
+      pulseCircles: [],
     }
     this.artboardRef = React.createRef();
     this.onMouseDown = this.onMouseDown.bind(this);
@@ -84,22 +97,15 @@ class ArtBoard extends React.Component {
   }
 
   onMouseDown(e) {
-    const newPulseCircle = [{x: this.alert_coords(e).x, y: this.alert_coords(e).y }]
-    this.setState({ pulseCircles: this.state.pulseCircles.concat(newPulseCircle) });
-    console.log(this.state.pulseCircles);
-    if (this.state.pulseCircles.length > 8){
-      this.removeExcessCircles();
-    }
+      const newPulseCircle = [{x: this.alert_coords(e).x, y: this.alert_coords(e).y }]
+      this.setState({ 
+        pulseCircles: this.state.pulseCircles.concat(newPulseCircle),
+       });
   }
 
   removeExcessCircles = () => {
-    console.log(this.state);
-    const pulseCircles = this.state.pulseCircles;
-
     this.setState({
-      pulseCircles: pulseCircles.filter((pulseCircle, i) => {
-        return i !== 0
-      })
+      pulseCircles: []
     })
   }
 
@@ -107,8 +113,11 @@ class ArtBoard extends React.Component {
     return (
       <div className="artboard-container" onMouseDown={this.onMouseDown}> 
         <svg  viewBox="0 0 100 100" ref={this.artboardRef}>
-          <PulseCircles pulseCircles={this.state.pulseCircles}/>
+          <PulseCircles 
+          pulseCircles={this.state.pulseCircles}
+          />
         </svg>
+        <button onClick={this.removeExcessCircles}> Clear</button>
       </div>
     )
   }
