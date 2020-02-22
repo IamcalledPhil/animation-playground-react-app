3835bddd053d275f19413d56acebab5b3fcbb601
Philip Cohn <philip.cohn@speakap.nl>
2020-02-22 20:12:54 +0100

    i'm a beautiful butterfly '

--- music-vis-react-app/src/App.js
+++ music-vis-react-app/src/App.js
@@ -6,34 +6,86 @@ import PulseCircles from './components/PulseCircles';
 import RandomAnts from './components/RandomAnts';
 
 
-class FloatingBubble extends React.Component {
+class Butterfly extends React.Component {
 
-  getRandomInt() {
-    return Math.floor(Math.random() * 100);
-  }
-
-  //make draggable
-  render() {
-    
+  renderButterflyWing(originX, originY, tx, rotation, opacity, rx, ry){
     return (
-        <motion.div className="floating-bubble"
-          border='#ffffff'
-          x={this.props.x}
-          y={this.props.y}
-          border-radius='5'
-          width='20'
-          height='20'
-          animate={{rotate: [ 270, 0]}}
-          drag
+      <g>
+        <motion.ellipse className="butterfly-wing"
+          fill={this.props.colour}
+          cx={this.props.x - tx}
+          cy={this.props.y}
+          opacity={opacity}
+          style={{ originX: originX, originY: originY} }
+          rx={rx}
+          ry={ry}
+          animate={{rotate: rotation, translateY: [1, 0, 1.3, 0, 0.5, 0, 1]}}
           transition={{
-            duration: 2,
-            ease: "linear",
-            loop: "infinity"
+            duration: 1.5,
+            loop: Infinity,
+            times: [0, 0.35, 0.4, 0.73, 0.8, 0.95, 1]
           }}
         />
+      </g>
+       
     )
   }
 
+  render() {
+    return(
+    <motion.g
+      animate={{translateX: [0, 10, -10, 5, 20, -2, -1, 10], 
+                translateY: [0, 5, -2, 10, -4, 25, 2, 5],
+                scale: [1, 0.6, 1, 0.7, 1, 0.5, 1]
+              }}
+      transition={{
+        duration: 12,
+        yoyo: Infinity
+      }}
+    >
+      {this.renderButterflyWing(0, 0.5, 0, [ 40, -20, 40, -20, 40, -20, 40], 1, 2, 1)}
+      {this.renderButterflyWing(1, 0.5, 4.3, [ -40, 20, -40, 20, -40, 20, -40], 1, 2, 1)}
+      {this.renderButterflyWing(0, 0.5, 0, [ 40, -20, 40, -20, 40, -20, 40], 0.4, 4, 2)}
+      {this.renderButterflyWing(1, 0.5, 4.3, [ -40, 20, -40, 20, -40, 20, -40], 0.4, 4, 2)}
+    </motion.g>
+
+    )
+  }
+}
+
+class Butterflies extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.colours = [
+      '#FF5B17',
+      '#E87115',
+      '#FFA423',
+      '#E8A515',
+      '#FFCD17'
+    ]
+  }
+
+  render(){
+    let colourIndex = 0;
+    if (this.props.butterflies.length > 0){
+      const butterflies = this.props.butterflies.map((butterfly, index) => {
+        colourIndex = colourIndex >= this.colours.length-1 ? 0 : colourIndex +1;
+        return (
+          <Butterfly 
+            key={index} 
+            index={index}
+            x={butterfly.x} 
+            y={butterfly.y} 
+            colour={this.colours[colourIndex]}
+          />
+        )
+      })
+      return butterflies;
+    } else {
+      return null;
+    }
+  }
 }
 
 class ArtBoard extends React.Component {
@@ -43,7 +95,7 @@ class ArtBoard extends React.Component {
     this.initialState = {
       pulseCircles: [],
       randomAnts: [],
-      floatingBubble: false,
+      butterflies: [],
       currentTool: 'pulseCircles'
     }
     this.state = this.initialState;
@@ -78,10 +130,10 @@ class ArtBoard extends React.Component {
         this.setState({ 
           randomAnts: this.state.randomAnts.concat(newElement),
          });
-      } else if (this.state.currentTool === 'floatingBubble')
+      } else if (this.state.currentTool === 'butterflies')
       {
         this.setState({ 
-          floatingBubble: newElement,
+          butterflies: this.state.butterflies.concat(newElement),
          });
       }
       
@@ -107,14 +159,14 @@ class ArtBoard extends React.Component {
           <RandomAnts 
           randomAnts={this.state.randomAnts}
           />
-          <FloatingBubble 
-          x={this.state.floatingBubble.x} y={this.state.floatingBubble.y}
+          <Butterflies
+          butterflies={this.state.butterflies}
           />
         </svg>
         <button onClick={this.clear}> Clear</button>
         <button onClick={() => {this.selectTool("pulseCircles")}}>Circles</button>
         <button onClick={() => {this.selectTool("randomAnts")}}>Ants</button>
-        <button onClick={() => {this.selectTool("floatingBubble")}}>Floating bubble</button>
+        <button onClick={() => {this.selectTool("butterflies")}}>Butterflies</button>
       </div>
     )
   }
