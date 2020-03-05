16c9c7d383a03b8646da980a3a6c3bea94f0a014
Philip Cohn <philip.cohn@speakap.nl>
2020-03-05 23:58:15 +0100

    started about section

---
+++ music-vis-react-app/src/components/AboutSection.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class AboutSection extends React.Component {
+  
+    render () {
+      return (
+        <div className="about-section">
+            <p>This project is an experimental art project that allows everyone to 
+                create their own animated visualisations. It is built in React, and 
+                uses the animation library Framer Motion. 
+                
+                Author: Phil Cohn </p> 
+        </div>
+      )
+    }
+  }
+
+  export default AboutSection;
--- music-vis-react-app/src/components/App.js
+++ music-vis-react-app/src/components/App.js
@@ -1,15 +1,45 @@
 import React from 'react';
 
 import ArtBoard from './Artboard';
+import AboutSection from './AboutSection';
+
+import '../styles/index.scss';
 
 class App extends React.Component {
 
+  constructor(props){
+    super(props)
+    this.initialState = {
+      activePage: 'artboard'
+    }
+    this.state = this.initialState;
+  }
+
+
+  goToAboutPage = () => {
+    this.setState({ activePage: "about"})
+  }
+
+  goToArtboard = () => {
+    this.setState({ activePage: "artboard"})
+  }
+
   render () {
-    return (
-      <div className="App">
-        <ArtBoard />
-      </div>
-    );
+    if (this.state.activePage === "artboard"){
+      return (
+        <div className="app">
+          <button onClick={this.goToAboutPage}>About</button>
+          <ArtBoard updateActivePage={this.updateActivePage}/>
+        </div>
+      ) 
+    } else if (this.state.activePage === "about"){
+      return(
+        <div className="app">
+          <button onClick={this.goToArtboard}>Back</button>
+          <AboutSection updateActivePage={this.updateActivePage}/>
+        </div>
+      )
+    }
   }
 }
 
